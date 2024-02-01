1908862f87b5f15ffa89dd942ad24dc02e48760e
LIBERZONE <mauniclean@gmail.com>
2024-02-01 15:10:30 -0500

    added locators for register.page

--- tests/regisration.spec.js
+++ tests/regisration.spec.js
@@ -1,4 +1,7 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 test('login',async ({page, baseURL}) => {
     await page.goto(baseURL);
-} )
+
+    await page.locator("(//li[@class='dropdown'])[1]").click();
+    await page.locator("(//a[contains(text(),'Register')])").click();
+    await expect(page).toHaveURL("https://eurosmeta.com/index.php?route=account/register")}); 
